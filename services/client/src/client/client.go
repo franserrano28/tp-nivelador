49e72bca71dd6186d594a21ebf45e3fa3d37d457
Francisco Serrano <franciscoserrano@MacBook-Air-de-Francisco.local>
2026-09-10 16:34:36 -0300

    Probando con mas retries y sleep

--- services/client/src/client/client.go
+++ services/client/src/client/client.go
@@ -13,8 +13,8 @@ import (
 	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/protocol"
 )
 
-const CONNECTION_ATTEMPTS_MAX = 3
-const CONNECTION_ATTEMPS_DELAY_MS = 200
+const CONNECTION_ATTEMPTS_MAX = 15
+const CONNECTION_ATTEMPS_DELAY_MS = 300
 
 type ClientConfig struct {
 	ServerHost string
